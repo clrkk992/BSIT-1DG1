9c8713858a155960d062f06a17a8f4a0bc0b6621
Baguio, Janfred Clark <clark.janfredbaguio@gmail.com>
2026-02-04 19:38:04 +0800

    GradeAnalyzer.java

--- GradeAnalyzer.java
+++ GradeAnalyzer.java
@@ -4,21 +4,21 @@ class GradeAnalyzer {
     public static void main(String[] clrk) {
         Scanner inpt = new Scanner(System.in);
 
-	int[] grades = new int[10];
-	int sum=0, highest = grades[0];
-	double average;
+		int[] grades = new int[10];
+		int sum=0, highest = grades[0];
+		double average;
 
-	System.out.println("Enter Grades: ");
-	for (int i = 0; i < grades.length; i++) {
-	    grades[i] = inpt.nextInt();
+		System.out.println("Enter Grades: ");
+		for (int i = 0; i < grades.length; i++) {
+	    	grades[i] = inpt.nextInt();
 
-	    sum += grades[i];
-	    if (grades[i] > highest)
-	        highest = grades[i];
-	}
+	    	sum += grades[i];
+	    	if (grades[i] > highest)
+	        	highest = grades[i];
+		}
 
-	average = (double) sum / grades.length;
-	System.out.println("Average Grade: " + average);
-	System.out.println("Highest Grade: " + highest);
+		average = (double) sum / grades.length;
+		System.out.println("Average Grade: " + average);
+		System.out.println("Highest Grade: " + highest);
     }
 }
