1d5370648be0808a2caee96a0d895a295131955d
Baguio, Janfred Clark <clark.janfredbaguio@gmail.com>
2026-02-04 19:20:17 +0800

    MathOper2.java

--- MathOper2.java
+++ MathOper2.java
@@ -4,26 +4,17 @@ class MathOper2 {
     static Scanner inpt = new Scanner(System.in);
 
     public static void main(String[] clrk) {
-        int N1, N2, Sum, Diff, Prod, Quot, Rem;
+        int N1, N2;
 	
-	System.out.print("Enter first number: ");
-	N1 = inpt.nextInt();
-	System.out.print("Enter second number: ");
-	N2 = inpt.nextInt();
+		System.out.print("Enter first number: ");
+		N1 = inpt.nextInt();
+		System.out.print("Enter second number: ");
+		N2 = inpt.nextInt();
 
-	//Sum = N1 + N2;
     	System.out.println("Sum = " + Sum(N1, N2));
-
-    	//Diff = N1 - N2;
     	System.out.println("Difference = " + Diff(N1, N2));
-
-    	//Prod = N1 * N2;
     	System.out.println("Product = " + Prod(N1, N2));
-
-    	//Quot = N1 / N2;
     	System.out.println("Quotient = " + Quot(N1, N2));
-
-    	//Rem = N1 % N2;
     	System.out.println("Remainder = " + Rem(N1, N2));
     }
 
@@ -36,4 +27,5 @@ class MathOper2 {
     static int Quot(int x, int y)  { return x / y; }
 
     static int Rem(int x, int y)   { return x % y; }
+
 }
