b5a5c14135dabc100b76b789a0d7a6526fd1aee7
Baguio, Janfred Clark <clark.janfredbaguio@gmail.com>
2026-02-05 16:45:03 +0800

    PE02JCB.java

--- PE02JCB.java
+++ PE02JCB.java
@@ -10,16 +10,16 @@ class PE02JCB {
     public static void main(String[] clrk) {
         Scanner inpt = new Scanner(System.in);
 	
-	int num;
+		int num;
 
-	System.out.print("Enter a number: ");
-	num = inpt.nextInt();
-
-	if (num > 0) 
-	    System.out.println("Positive");
-	else if (num < 0)
-	    System.out.println("Negative");
-	else
-	    System.out.println("Zero");
+		System.out.print("Enter a number: ");
+		num = inpt.nextInt();
+	
+		if (num > 0) 
+		    System.out.println("Positive");
+		else if (num < 0)
+		    System.out.println("Negative");
+		else
+		    System.out.println("Zero");
     }
 }
