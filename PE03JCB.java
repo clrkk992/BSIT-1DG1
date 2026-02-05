2a72b1ffa3d3dac7578fc9db1d9b7215262d39e6
Baguio, Janfred Clark <clark.janfredbaguio@gmail.com>
2026-02-05 16:45:53 +0800

    PE03JCB.java

--- PE03JCB.java
+++ PE03JCB.java
@@ -10,18 +10,18 @@ class PE03JCB {
     public static void main(String[] clrk) {
         Scanner inpt = new Scanner(System.in);
 	
-	int n1, n2;
-
-	System.out.print("Enter 1st number: ");
-	n1 = inpt.nextInt();
-	System.out.print("Enter 2nd number: ");
-	n2 = inpt.nextInt();
-
-	if (n1 > n2) 
-	    System.out.println(n1 + " is greater than " + n2);
-	else if (n1 < n2)
-	    System.out.println(n1 + " is less than " + n2);
-	else
-	    System.out.println(n1 + " is equal to " + n2);
+		int n1, n2;
+	
+		System.out.print("Enter 1st number: ");
+		n1 = inpt.nextInt();
+		System.out.print("Enter 2nd number: ");
+		n2 = inpt.nextInt();
+	
+		if (n1 > n2) 
+		    System.out.println(n1 + " is greater than " + n2);
+		else if (n1 < n2)
+		    System.out.println(n1 + " is less than " + n2);
+		else
+		    System.out.println(n1 + " is equal to " + n2);
     }
 }
